1c52e63a75e117736dbadf5df08bb12e4bb451bc
Darkythan <manelqf@gmail.com>
2024-10-27 15:44:48 +0100

    Create and setup GameMode

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Manel Quintana
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Manel Quintana
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
